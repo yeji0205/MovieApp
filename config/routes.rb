56ecd99a51f7da0760df7608d38b15be76391572
Yegi Park <yehji205@gmail.com>
2019-08-07 12:38:32 +0200

    Actors and movies can be modeled using a many-to-many relationship. In rails, this can be implemented using a has_many:through association. The has_many :through association joins two models via a third model.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   get '/movies' => 'movies#index'
   get '/movies/1' => 'movies#show', as: :movie
+  get '/actors' => 'actors#index'
+  get '/actors/1' => 'actors#show', as: :actor
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
